a6b070c07689318c91c5d0848a8c830d506a6529
Enrj-Drunk <99850420+Imaginit22@users.noreply.github.com>
2025-05-07 08:07:52 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from ctypes import windll
 
 #this is a py file
 print("Hello guys whats up it is I Noah")
@@ -13,11 +14,14 @@ def test1():
     # root window title and dimension
     root.title("Thomas might be the gote")
     # Set geometry (widthxheight)
-    root.geometry('100x100')
+    root.geometry('100x100+65+400')
     hi = Label(root, text="He")
     hi.pack()
     # all widgets will be here
     # Execute Tkinter
-    root.mainloop()
-
+    try:
+        from ctypes import windll
+        windll.shcore.SetProcessDpiAwareness(1)
+    finally:
+        root.mainloop()
 test1()
