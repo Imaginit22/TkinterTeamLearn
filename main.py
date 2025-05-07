7ee69057f748e84203d04b69e21966635304fc1f
Thomas Brittain <thomas.brittain3@gmail.com>
2025-05-07 10:39:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+from tkinter import *
+
 #this is a py file
 print("Hello guys whats up it is I Noah")
+
+#I'm just making sure I remeber how to use github desktop
+#i purposely spelled remeber remeber
+
+def test1():
+    # create root window
+    root = Tk()
+    # root window title and dimension
+    root.title("Thomas might be the gote")
+    # Set geometry (widthxheight)
+    root.geometry('100x100')
+
+    # all widgets will be here
+    # Execute Tkinter
+    root.mainloop()
+
+test1()
